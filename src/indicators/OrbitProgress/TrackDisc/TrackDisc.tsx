58f79a3e4c17f48b21a84ab94cfeff699f1e80f2
smitter <hanesmitter3@gmail.com>
2024-01-29 09:44:33 +0300

    Add dense functionality responsiveness on OP:variant="track-disc" [rebase]

--- src/indicators/OrbitProgress/TrackDisc/TrackDisc.tsx
+++ src/indicators/OrbitProgress/TrackDisc/TrackDisc.tsx
@@ -20,7 +20,8 @@ const annulusTrackColorVars: Array<string[]> = Array.from(
 );
 const ANNULUS_TRACK_ALPHA__: number = 0.25; // 0 - 1
 const alphaSetter: (num?: number) => number = (originalAlpha: number = 1) => {
-	const derived = originalAlpha * 0.25;
+	const derived = originalAlpha * ANNULUS_TRACK_ALPHA__;
+	console.log({ originalAlpha, "derived-Alpha": derived });
 	return derived;
 };
 
@@ -67,6 +68,7 @@ const TrackDisc = (props: TrackDiscProps) => {
 	let colorProp: string | string[] = props?.color ?? "";
 	const annulusTrackColorStyles: React.CSSProperties =
 		stylesObjectFromColorProp(colorProp, colorReset);
+	const boldface: string = props.dense ? "0.45em" : "";
 
 	return (
 		<span
@@ -89,7 +91,10 @@ const TrackDisc = (props: TrackDiscProps) => {
 				ref={elemRef}
 				style={{ ...annulusTrackColorStyles, ...styles }}
 			>
-				<span className="rli-d-i-b annulus-track-ring"></span>
+				<span
+					className="rli-d-i-b annulus-track-ring"
+					style={{ ...(boldface && { borderWidth: boldface }) }}
+				></span>
 
 				<Text
 					className="OP-annulus-sector-text"
